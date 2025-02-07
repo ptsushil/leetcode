0ea6c768a982a3b797319a1e64c72ada7f92eb0a
ssharma <ssharma@biglots.com>
2025-02-07 18:35:13 -0500

    Binary search

---
+++ src/main/java/com/biglots/com/leetcode/model/BinarySearch.java
@@ -0,0 +1,12 @@
+package com.biglots.com.leetcode.model;
+
+import java.util.Arrays;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 3, 1, 2, 6, 7, 8, 9};
+        System.out.println(Arrays.toString(arr));
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
